a9717965a4558b94c057972941f74b7a39338cb6
Anh Khoa Võ <voanhkhoa2211@hotmail.com>
2018-06-17 23:22:39 +0700

    LotteryStatus Class

---
+++ FinalProject/app/src/main/java/com/example/huykhoahuy/finalproject/Class/LotterStatus.java
@@ -0,0 +1,42 @@
+package com.example.huykhoahuy.finalproject.Class;
+
+import java.util.Date;
+
+public class LotterStatus {
+    private String provice_id;
+    private String yourcode;
+    private Date date;
+    private Boolean yourresult;
+
+    public String getProvice_id() {
+        return provice_id;
+    }
+
+    public void setProvice_id(String provice_id) {
+        this.provice_id = provice_id;
+    }
+
+    public String getYourcode() {
+        return yourcode;
+    }
+
+    public void setYourcode(String yourcode) {
+        this.yourcode = yourcode;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Boolean getYourresult() {
+        return yourresult;
+    }
+
+    public void setYourresult(Boolean yourresult) {
+        this.yourresult = yourresult;
+    }
+}
